a037cf62940bddd68ebefc67f08cf121c3e99374
Jake Bott <jake.anq@gmail.com>
2023-09-09 14:20:20 +0930

    Apply prettier to existing files

--- Frontend/src/index.ts
+++ Frontend/src/index.ts
@@ -1,10 +1,13 @@
-import "./index.scss"
-import EM from "./Assets/Images/EM.svg"
+import "./index.scss";
+import EM from "./Assets/Images/EM.svg";
 
 // Simple test page with the Excessive Motion logo and some animations + text
 fetch(EM).then((resp) => {
-    resp.text().then(svg => {
-        document.body.classList.add("noselect")
-        document.body.innerHTML = `<div class="em-test"><div>${svg}${svg.replaceAll("<svg", "<svg class=\"sweep\"")}</div><span>Excessive Motion UI</span></div>`
-    })
-})
+    resp.text().then((svg) => {
+        document.body.classList.add("noselect");
+        document.body.innerHTML = `<div class="em-test"><div>${svg}${svg.replaceAll(
+            "<svg",
+            '<svg class="sweep"',
+        )}</div><span>Excessive Motion UI</span></div>`;
+    });
+});
